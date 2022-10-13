2a85408089337535796f0cd02a280d3c8ad59c46
Alumno <alumno@sistemas-utnfra.com>
2022-10-13 19:06:32 -0300

    compoenent tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltroComponent } from './filtro/filtro.component';
+import { TablaComponent } from './tabla/tabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltroComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule,
